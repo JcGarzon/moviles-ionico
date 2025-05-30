74f315b668b4000a621e8e02f052c75ceaaf77e2
Sacol16 <santiago.colonia@uao.edu.co>
2025-05-30 17:37:38 -0500

    tabs

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -17,5 +17,17 @@ export const routes: Routes = [
     loadChildren: () =>
       import('./tabs/tabs.routes').then((m) => m.routes),
     canActivate: [AuthGuard]
+  },  {
+    path: 'tab4',
+    loadComponent: () => import('./tab4/tab4.page').then( m => m.Tab4Page)
   },
+  {
+    path: 'tab5',
+    loadComponent: () => import('./tab5/tab5.page').then( m => m.Tab5Page)
+  },
+  {
+    path: 'tab6',
+    loadComponent: () => import('./tab6/tab6.page').then( m => m.Tab6Page)
+  },
+
 ];
